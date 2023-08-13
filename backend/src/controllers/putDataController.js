039090690b9f84f8bf39f891532fe85dc7c87c55
ADEP-123 <andep210399@gmail.com>
2023-08-13 16:33:27 -0500

    fix: :bug: Arreglando que no se devolvia el id de el inventario creado

--- backend/src/controllers/putDataController.js
+++ backend/src/controllers/putDataController.js
@@ -16,8 +16,9 @@ const putInventarioController = async (req, res, next) => {
                     result = await putCantidadProductInventarioService(bodega2, producto, (cantidadExistenteB2 + cantidad))
                     res.status(200).json({ message: "Se han actualizado los inventarios con exito" })
                 } else {
-                    result = await postInventarioService(123456789, bodega2, producto, cantidad, usuario)
-                    res.status(200).json({ message: "Se ha actualizado 1 inventario y creado un inventario nuevo" })
+                    result = await postInventarioService(bodega2, producto, cantidad, usuario)
+                    const id = result.insertedId;
+                    res.status(200).json({ message: `Se ha actualizado 1 inventario y creado un inventario nuevo ${id}` })
                 }
             } else {
                 res.status(500).json({ message: `La cantidad existente (${cantidadExistenteB1}) en la bodega de salida es menor a la que desea mover (${cantidad})` })
