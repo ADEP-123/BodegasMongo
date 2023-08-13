43338fb239e3284d090b94dea022a38d24299323
ADEP-123 <andep210399@gmail.com>
2023-08-13 17:16:42 -0500

    feat: :sparkles: middleware rate limit

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import getInitRoute from "./getData.js";
 import postInitRoute from "./postData.js";
 import putInitRoute from "./putData.js";
+import { middlewareRateLimit } from "../middleware/limit.js";
 
 const initApiRoutes = () => {
     const router = Router();
-    router.use("/get", getInitRoute())
-    router.use("/post", postInitRoute())
-    router.use("/put", putInitRoute())
+    router.use("/get", middlewareRateLimit, getInitRoute())
+    router.use("/post", middlewareRateLimit, postInitRoute())
+    router.use("/put", middlewareRateLimit, putInitRoute())
     return router
 }
 
