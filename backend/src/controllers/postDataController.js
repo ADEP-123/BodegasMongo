87a9aeae341ecef022318849089488c0d63b4997
ADEP-123 <andep210399@gmail.com>
2023-08-13 12:00:23 -0500

    fix: :bug: Arreglando bug en endpoint 4 que permitia crear un nuevo producto con id de creador inexistente

--- backend/src/controllers/postDataController.js
+++ backend/src/controllers/postDataController.js
@@ -8,12 +8,10 @@ const postBodegaController = async (req, res, next) => {
         if (comprobarCreador != 0) {
             const result = await postBodegaService(id, nombre, responsable, estado, creador)
             res.status(200).json({ message: "Registro insertado con exito", result })
-            
+
         } else {
             res.status(500).json({ error: `Imposible realizar la insercion: el creador identificado con el id ${creador} no existe` })
-
         }
-        console.log(comprobarCreador);
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
@@ -21,11 +19,16 @@ const postBodegaController = async (req, res, next) => {
 
 const postProductoController = async (req, res, next) => {
     try {
-        const { id, nombre, descripcion, estado, creador } = req.body
-        let result = await postProductoService(id, nombre, descripcion, estado, creador);
-        if (result.acknowledged == true) {
-            result = await postInventarioService(1234567, 12, id, 100, creador)
-            res.status(200).json({ message: `El producto ${nombre} identificado con id: ${id} fue ingresado con exito en la bodega por defecto 12, con una cantidad inicial de 100` })
+        const { id, nombre, descripcion, estado, creador } = req.body;
+        const comprobarCreador = await comprobarUsuarioService(creador)
+        if (comprobarCreador != 0) {
+            let result = await postProductoService(id, nombre, descripcion, estado, creador);
+            if (result.acknowledged == true) {
+                result = await postInventarioService(9999995522, 12, id, 100, creador)
+                res.status(200).json({ message: `El producto ${nombre} identificado con id: ${id} fue ingresado con exito en la bodega por defecto 12, con una cantidad inicial de 100` })
+            }
+        } else {
+            res.status(500).json({ error: `Imposible realizar la insercion: el creador identificado con el id ${creador} no existe` })
         }
     } catch (error) {
         res.status(500).json({ error: error.message })
