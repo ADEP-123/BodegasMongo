86836dd24cd8f438db8856f1fcad6d8ee8313bf0
ADEP-123 <andep210399@gmail.com>
2023-08-15 07:51:50 -0500

    feat: :bug: JWT con endpoint generador de token y middleware de autentificacion de la autorizacion

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,12 +3,15 @@ import getInitRoute from "./getData.js";
 import postInitRoute from "./postData.js";
 import putInitRoute from "./putData.js";
 import { middlewareRateLimit } from "../middleware/limit.js";
+import { appToken } from "../services/tokenGenerator.js";
+import { authorizationMiddleware } from "../middleware/autohorizationMiddleware.js";
 
 const initApiRoutes = () => {
     const router = Router();
-    router.use("/get", middlewareRateLimit, getInitRoute())
-    router.use("/post", middlewareRateLimit, postInitRoute())
-    router.use("/put", middlewareRateLimit, putInitRoute())
+    router.use("/login", appToken)
+    router.use("/get", authorizationMiddleware, middlewareRateLimit, getInitRoute())
+    router.use("/post", authorizationMiddleware, middlewareRateLimit, postInitRoute())
+    router.use("/put", authorizationMiddleware, middlewareRateLimit, putInitRoute())
     return router
 }
 
---
+++ backend/src/services/tokenGenerator.js
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+import { plainToClass, classToPlain } from 'class-transformer';
+import dotenv from 'dotenv';
+import { Router } from 'express';
+import { SignJWT } from 'jose';
+import { bodegasDTO } from '../routes/dto/js/bodegaDTO.js';
+import { inventariosDTO } from '../routes/dto/js/inventariosDTO.js';
+import { productosDTO } from '../routes/dto/js/productosDTO.js';
+import { updateInventariosDTO } from '../routes/dto/js/updateInventariosDTO.js';
+
+dotenv.config();
+const appToken = Router();
+
+appToken.use("/:collecion", async (req, res) => {
+    try {
+        let inst = plainToClass(eval(req.params.collecion), {}, { ignoreDecorators: true })
+        const encoder = new TextEncoder();
+        const jwtconstructor = new SignJWT(Object.assign({}, classToPlain(inst)));
+        const jwt = await jwtconstructor
+            .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+            .setIssuedAt()
+            .setExpirationTime("30m")
+            .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+        req.data = jwt;
+        res.status(201).send({ status: 201, message: jwt });
+    } catch (error) {
+        console.log(error);
+        res.status(404).send({ status: 404, message: "Token solicitado no valido" });
+    }
+})
+
+export {
+    appToken,
+};
